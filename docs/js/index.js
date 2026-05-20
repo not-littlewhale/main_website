8383e620d569724653ddad9d8b9efa66007e066c
Little Whale <whalechen523@gmail.com>
2026-05-20 07:41:29 +0000

    the random quote function works now - just not set up design wise probably needs more thinking so not gonna add it in yet, but the code is there and it works.

--- docs/js/index.js
+++ docs/js/index.js
@@ -4,21 +4,23 @@
 // js list of quotes (database maybe), randomize, change the text of the quote element to the quote
 // not quote of the "day" but quote per refresh, some call it false advertising I call it more interesting
 
-const modifying_text = document.querySelector('.changable-text');
-const quotes = [
-    "quote 1",
-    "quote 2",  // properly implmenting these later
-] 
-// they're called quotes but ig we can also do fun facts and silly messages
+document.addEventListener('DOMContentLoaded', function() {
+    const modifying_text = document.querySelector(".changable-text");
+    const quotes = [
+        "quote 1",
+        "quote 2",  // properly implmenting these later
+    ] 
+    // they're called quotes but ig we can also do fun facts and silly messages
 
-function getRandomQuote() {
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    return quotes[randomIndex];
-}  // UNTESTED - assuming this works for now
+    function getRandomQuote() {
+        const randomIndex = Math.floor(Math.random() * quotes.length);
+        return quotes[randomIndex];
+    }  // UNTESTED - assuming this works for now
 
-function setQuote(){
-    const quote = getRandomQuote();
-    modifying_text.textContent = quote;
-}
+    function setQuote(){
+        const quote = getRandomQuote();
+        modifying_text.textContent = quote;
+    }
 
-setQuote();  // TODO: broken. for some goddamn reason
+    setQuote();
+});
